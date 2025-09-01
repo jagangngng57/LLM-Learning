50af42e2e87f675a1be627e2494dcd416cab2ffc
Jagadeesh gyangi <jaibhargavmanu@Jagadeeshs-MacBook-Pro.local>
2025-08-31 22:33:37 -0700

    added some more basics

---
+++ PythonBasics/delVsNone.py
@@ -0,0 +1,8 @@
+#none and del keyword program
+x=10
+y=10
+z=10
+del x  # Delete the variable x
+print("Value of y:",y)  # This will print 10
+print("Value of z:",z)  # This will print 10  
+#print("Value of x:",x)  # This will raise an error since x is deleted
